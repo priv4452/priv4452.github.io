cbf27a86c755b8fe576f916e324607d843c284ea
priv4452 <159673507+priv4452@users.noreply.github.com>
2024-02-10 19:03:27 -0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,1 +1,10 @@
-
+<?php
+if ($_FILES["image"]["error"] == UPLOAD_ERR_OK) {
+  $tempFilePath = $_FILES["image"]["tmp_name"];
+  $newFilePath = "uploads/" . $_FILES["image"]["name"];
+  move_uploaded_file($tempFilePath, $newFilePath);
+  echo "Image uploaded successfully!";
+} else {
+  echo "Error occurred while uploading the image.";
+}
+?>
